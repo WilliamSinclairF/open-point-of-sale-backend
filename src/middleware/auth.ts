4dd1737e50de2fcaa627771b9cb782abf89585ab
WilliamSinclairF <willzy@outlook.com>
2021-08-01 21:24:21 -0400

    deployment changes

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,18 +3,7 @@ import { User } from './../entity/User';
 import { NextFunction, Request, Response } from 'express';
 import admin, { ServiceAccount } from 'firebase-admin';
 
-const serviceAccount = {
-  type: process.env.FB_TYPE,
-  project_id: process.env.FB_PROJECT_ID,
-  private_key_id: process.env.FB_PRIVATE_KEY_ID,
-  private_key: process.env.FB_PRIVATE_KEY,
-  client_email: process.env.FB_CLIENT_EMAIL,
-  client_id: process.env.FB_CLIENT_ID,
-  auth_uri: process.env.FB_AUTH_URI,
-  token_uri: process.env.FB_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER,
-  client_x509_cert_url: process.env.FB_CLIENT_CERT_URL,
-};
+import serviceAccount from '../firebase-admin.json';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as ServiceAccount),
